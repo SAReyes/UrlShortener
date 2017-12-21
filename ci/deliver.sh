25bd846d9401fa59a76385c3bb72e10e1e55b1dd
Adrian Reyes <sadrian.reyes@gmail.com>
2017-12-21 05:16:06 +0100

    docker repo lowercase

--- ci/deliver.sh
+++ ci/deliver.sh
@@ -1,7 +1,7 @@
 ./gradlew :urlshortener-configuration:bootJar
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 export TAG=`if [ "$BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-export REPO=SAReyes/urlshortener-be
+export REPO=sareyes/urlshortener-be
 docker build -f Dockerfile -t $REPO:$COMMIT .
 docker tag $REPO:$COMMIT $REPO:$TAG
 docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
@@ -10,7 +10,7 @@ docker push $REPO
 cd ui
 npm i
 npm run-script build
-export REPO=SAReyes/urlshortener-fe
+export REPO=sareyes/urlshortener-fe
 docker build -f Dockerfile -t $REPO:$COMMIT .
 docker tag $REPO:$COMMIT $REPO:$TAG
 docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
