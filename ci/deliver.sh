d7345fc768843977f4168cb6cca73d73b6b94a9f
Adrian Reyes <sadrian.reyes@gmail.com>
2017-12-21 05:08:48 +0100

    updating docker user

--- ci/deliver.sh
+++ ci/deliver.sh
@@ -1,17 +1,16 @@
 ./gradlew :urlshortener-configuration:bootJar
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 export TAG=`if [ "$BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-export REPO=sareyes/urlshortener-be
+export REPO=SAReyes/urlshortener-be
 docker build -f Dockerfile -t $REPO:$COMMIT .
 docker tag $REPO:$COMMIT $REPO:$TAG
 docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
 docker push $REPO
 
 cd ui
-nvm install 8.0.0
 npm i
 npm run-script build
-export REPO=sareyes/urlshortener-fe
+export REPO=SAReyes/urlshortener-fe
 docker build -f Dockerfile -t $REPO:$COMMIT .
 docker tag $REPO:$COMMIT $REPO:$TAG
 docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
