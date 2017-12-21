56fc32041513e3409a5b310dd55f894fd3ec53bd
Adrian Reyes <sadrian.reyes@gmail.com>
2017-12-21 04:43:39 +0100

    Docker delivery

---
+++ ci/deliver.sh
@@ -0,0 +1,17 @@
+./gradlew :urlshortener-configuration:bootJar
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+export TAG=`if [ "$BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+export REPO=sareyes/urlshortener-be
+docker build -f Dockerfile -t $REPO:$COMMIT .
+docker tag $REPO:$COMMIT $REPO:$TAG
+docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+docker push $REPO
+
+cd ui
+npm i
+npm run-script build
+export REPO=sareyes/urlshortener-fe
+docker build -f Dockerfile -t $REPO:$COMMIT .
+docker tag $REPO:$COMMIT $REPO:$TAG
+docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+docker push $REPO
